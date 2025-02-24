d8fd0892ebec08c7489cdf9b2ca8cd4a2a287d71
yashvardhan <yashvardhansinhjadeja187@gmail.com>
2025-02-24 22:18:49 +0530

    Update game.cpp
    As asked, the change includes implementation of the determineWinner function, which compares the player's choice and the computer's choice to determine the winner.

--- game.cpp
+++ game.cpp
@@ -12,7 +12,17 @@ public:
 
     void getComputerChoice(){}  // Shubham
 
-    void determineWinner(){} // Jadeja & Kamil
+     void determineWinner() { // Jadeja & Kamil
+        if (playerChoice == computerChoice) {
+            cout << "It's a tie!" << endl;
+        } else if ((playerChoice == 0 && computerChoice == 2) ||
+                   (playerChoice == 1 && computerChoice == 0) ||
+                   (playerChoice == 2 && computerChoice == 1)) {
+            cout << "Player wins!" << endl;
+        } else {
+            cout << "Computer wins!" << endl;
+        }
+    }
 
     string choiceToString(int choice){} // Mann & Harsh
 };
